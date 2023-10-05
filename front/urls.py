0fcb777e028f5300d431b3fa47c5bd8c8e6fcb88
sf178 <dmitrynekrylov@yandex.ru>
2023-10-05 18:14:01 +0300

    Полностью прикручена аутентификация с правами

--- front/urls.py
+++ front/urls.py
@@ -10,12 +10,12 @@ router.register("profile", UserProfileView)
 # router.register("profile/up", UserProfilePartialUpdateView)
 urlpatterns = [
     path('', include(router.urls)),
-    path('login', LoginView.as_view()),
-    path('register', RegisterView.as_view()),
-    path('register/confirm', ConfirmRegistrationView.as_view()),
-    path('refresh', RefreshView.as_view()),
-    path('me', MeView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('register/', RegisterView.as_view()),
+    path('register/confirm/', ConfirmRegistrationView.as_view()),
+    path('refresh/', RefreshView.as_view()),
+    path('me/', MeView.as_view()),
     # path('profile/', UserProfileRetrieveUpdateView.as_view(), name='user-profile'),
     # path('profile/update/', UserProfilePartialUpdateView.as_view(), name='user-profile-partial-update'),
-    path('logout', LogoutView.as_view()),
+    path('logout/', LogoutView.as_view()),
 ]
