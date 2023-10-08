49e8a5b8761d6222b0a890f6c0b4cfa4fb635473
sf178 <dmitrynekrylov@yandex.ru>
2023-10-08 22:28:14 +0300

    Просмотр профиля

--- category/models.py
+++ category/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class ExpensePersonalCategory(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey('front.CustomUser', on_delete=models.CASCADE, null=True, blank=True)
-    is_default = models.BooleanField(default=False)
+    is_default = models.BooleanField(default=False, null=True)
     name = models.CharField(max_length=255, blank=True, null=True)
     def __str__(self):
         return self.name
@@ -13,7 +13,7 @@ class ExpensePersonalCategory(models.Model):
 class ExpenseGeneralCategory(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey('front.CustomUser', on_delete=models.CASCADE, null=True, blank=True)
-    is_default = models.BooleanField(default=False)
+    is_default = models.BooleanField(default=False, null=True)
     name = models.CharField(max_length=255, blank=True, null=True)
     def __str__(self):
         return self.name
