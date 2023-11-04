b7a12bf76bcb03580ac845f3a1b47ec54a33ddf5
sf178 <dmitrynekrylov@yandex.ru>
2023-11-05 03:46:51 +0400

    Исправил косяки, добавил приколов

--- receipt_scanner/urls.py
+++ receipt_scanner/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ListUserReceiptsView.as_view(), name='list-receipts'),
+    path('', views.ReceiptView.as_view(), name='list-receipts'),
     path('<int:pk>/', views.DeleteReceiptView.as_view(), name='delete-receipt'),
 ]
