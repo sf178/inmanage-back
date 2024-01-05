fc17e975a634ffa3263ccf308ff2981dc971a470
sf178 <dmitrynekrylov@yandex.ru>
2024-01-06 00:57:40 +0400

    micro апдейт

--- category/urls.py
+++ category/urls.py
@@ -5,11 +5,11 @@ from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('personal/', PersonalExpenseCategoryListCreateView.as_view(), name='personal-category-list'),
-    path('asset/', AssetCategoryListCreateView.as_view(), name='asset-category-list'),
-    path('liability/', LiabilityCategoryListCreateView.as_view(), name='liability-category-list'),
-    path('general/', ExpenseGeneralCategoryListView.as_view(), name='general-category-list'),
-    path('general/delete/<int:pk>/', ExpenseGeneralCategoryDeleteView.as_view(), name='general-category-delete'),
-    path('general/subcategories/delete/<int:pk>/', ExpenseGeneralSubcategoryDeleteView.as_view(), name='delete-general-subcategory'),
+    # path('asset/', AssetCategoryListCreateView.as_view(), name='asset-category-list'),
+    # path('liability/', LiabilityCategoryListCreateView.as_view(), name='liability-category-list'),
+    # path('general/', ExpenseGeneralCategoryListView.as_view(), name='general-category-list'),
+    # path('general/delete/<int:pk>/', ExpenseGeneralCategoryDeleteView.as_view(), name='general-category-delete'),
+    # path('general/subcategories/delete/<int:pk>/', ExpenseGeneralSubcategoryDeleteView.as_view(), name='delete-general-subcategory'),
 
 ]
 
