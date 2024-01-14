9544d71dff245d93ded04845dab71f1de735205d
sf178 <dmitrynekrylov@yandex.ru>
2024-01-14 17:55:33 +0400

    micro апдейт

--- income_expenses/views.py
+++ income_expenses/views.py
@@ -2,6 +2,7 @@ from test_backend.custom_methods import IsAuthenticatedCustom
 from rest_framework import mixins, generics
 from .models import WorkIncome, Work, Project
 from .serializers import WorkIncomeSerializer, WorkSerializer, ProjectSerializer
+from rest_framework.exceptions import ValidationError
 
 
 class WorkIncomeListView(mixins.ListModelMixin, generics.GenericAPIView):
@@ -52,6 +53,20 @@ class WorkListView(mixins.ListModelMixin, generics.GenericAPIView):
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+
+        return self.perform_create(request, *args, **kwargs)
+
+    def perform_create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # Проверка на наличие 'user' в data перед сохранением
+        # Если 'user' уже присутствует, это может означать попытку инъекции данных, и следует вернуть ошибку
+        if 'user' in serializer.validated_data:
+            raise ValidationError("You cannot set the user manually.")
+        serializer.save(user=self.request.user)
 
 
 class WorkDetailView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
