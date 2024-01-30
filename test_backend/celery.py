493524e4e07dd12cf00ff307956e737dfa80cf9a
sf178 <dmitrynekrylov@yandex.ru>
2024-01-30 06:13:29 +0300

    Еще один апдейт

--- test_backend/celery.py
+++ test_backend/celery.py
@@ -1,12 +1,12 @@
-from __future__ import absolute_import, unicode_literals
-
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_backend.settings')
-
-app = Celery('test_backend')
-
-app.config_from_object('django.conf:settings', namespace='CELERY')
-
-app.autodiscover_tasks()
+# from __future__ import absolute_import, unicode_literals
+#
+# import os
+# from celery import Celery
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_backend.settings')
+#
+# app = Celery('test_backend')
+#
+# app.config_from_object('django.conf:settings', namespace='CELERY')
+#
+# app.autodiscover_tasks()
