5d0c80f623d1c5e1b8ff7fabb4b7e5b39f64ccaf
sf178 <dmitrynekrylov@yandex.ru>
2024-02-01 00:18:26 +0300

    Еще один апдейт

--- balance/views.py
+++ balance/views.py
@@ -20,12 +20,17 @@ import json
 class CurrencyListView(ListModelMixin, CreateModelMixin, GenericAPIView):
     queryset = Currency.objects.all()
     serializer_class = CurrencySerializer
+    permission_classes = [IsAuthenticatedCustom]
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+        serializer = self.get_serializer(data=request.data, many=isinstance(request.data, list))
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class PaymentListView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
