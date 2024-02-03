28eb34007ce4c271c3c650cef0d6aac2746bdea0
sf178 <dmitrynekrylov@yandex.ru>
2024-02-03 04:13:48 +0300

    Еще один апдейт

--- income_expenses/views.py
+++ income_expenses/views.py
@@ -68,11 +68,11 @@ class WorkIncomeListView(mixins.ListModelMixin, mixins.CreateModelMixin, generic
     #             raise ValidationError(income_serializer.errors)
 
     def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        # serializer = self.get_serializer(data=request.data)
+        # serializer.is_valid(raise_exception=True)
+        return self.create(self, request, *args, **kwargs)
+        # headers = self.get_success_headers(serializer.data)
+        # return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class WorkIncomeDetailView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
