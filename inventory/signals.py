548b168bfc38e05c7df44d951da109df548b0027
sf178 <dmitrynekrylov@yandex.ru>
2024-02-13 04:01:48 +0300

    Еще один апдейт

--- inventory/signals.py
+++ inventory/signals.py
@@ -1,10 +1,21 @@
-from django.db.models.signals import post_save, m2m_changed
+from django.db.models.signals import post_save, m2m_changed, pre_delete
 from django.dispatch import receiver
 
 from actives.models import Business
 from .models import InventoryAsset, Inventory
 from django.contrib.contenttypes.models import ContentType
 
+
+@receiver(pre_delete, sender=InventoryAsset)
+def update_inventory_total_cost_before_delete(sender, instance, **kwargs):
+    # Проверяем, связан ли asset с каким-либо инвентарем
+    inventory = Inventory.objects.filter(assets=instance).first()
+    if inventory:
+        # Вычитаем стоимость удаляемого asset из total_cost инвентаря
+        inventory.total_cost -= instance.price
+        inventory.save()
+        # Пересчитываем total_worth для связанного бизнеса, если есть
+        update_related_business_total_worth(inventory)
 
 @receiver(m2m_changed, sender=Inventory.assets.through)
 def recalculate_inventory_total_cost(sender, instance, action, **kwargs):
@@ -23,3 +34,11 @@ def update_business_total_worth(business_instance):
     # Обновление total_worth для Business, если необходимо
     business_instance.total_worth = business_instance.calculate_total_worth()  # Предполагается, что метод calculate_total_worth() существует
     business_instance.save()
+
+
+def update_related_business_total_worth(inventory):
+    if inventory.content_type and inventory.object_id:
+        business_model = inventory.content_type.model_class()
+        if issubclass(business_model, Business):
+            business_instance = business_model.objects.get(id=inventory.object_id)
+            update_business_total_worth(business_instance)
