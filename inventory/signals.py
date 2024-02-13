d604f59199aca8139c5f62ec1492e454e512b2e1
sf178 <dmitrynekrylov@yandex.ru>
2024-02-13 05:11:48 +0300

    Еще один апдейт

--- inventory/signals.py
+++ inventory/signals.py
@@ -5,6 +5,29 @@ from .models import InventoryAsset, Inventory
 from actives.models import Business
 from django.contrib.contenttypes.models import ContentType
 
+@receiver(pre_delete, sender=InventoryAsset)
+def update_inventory_total_cost_before_delete(sender, instance, **kwargs):
+    # Проверяем, связан ли asset с каким-либо инвентарем
+    inventory = Inventory.objects.filter(assets=instance).first()
+    if inventory:
+        # Вычитаем стоимость удаляемого asset из total_cost инвентаря
+        inventory.total_cost -= instance.price
+        inventory.save()
+        # Пересчитываем total_worth для связанного бизнеса, если есть
+        update_business_total_worth(inventory)
+
+@receiver(m2m_changed, sender=Inventory.assets.through)
+def recalculate_inventory_total_cost(sender, instance, action, **kwargs):
+    if action in ["post_add", "post_remove", "post_clear"]:
+        total_cost = sum(asset.price for asset in instance.assets.all())
+        instance.total_cost = total_cost
+        instance.save()
+
+        if instance.content_type and instance.object_id:
+            business_model = instance.content_type.model_class()
+            if issubclass(business_model, Business):
+                business_instance = business_model.objects.get(id=instance.object_id)
+                update_business_total_worth(business_instance)
 
 # @receiver(post_save, sender=InventoryAsset)
 # @receiver(post_delete, sender=InventoryAsset)
@@ -19,8 +42,6 @@ def update_inventory_total_cost(sender, instance, **kwargs):
         inventory.save()
         # Вызов функции обновления total_worth для связанного бизнеса
         update_business_total_worth(inventory)
-
-
 
 
 def update_business_total_worth(inventory):
