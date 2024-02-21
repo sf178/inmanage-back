0d64526f2462635466663321096bf742d70cf46a
sf178 <dmitrynekrylov@yandex.ru>
2024-02-22 03:39:24 +0400

    Еще один апдейт

--- inventory/signals.py
+++ inventory/signals.py
@@ -1,62 +1,40 @@
-from django.db.models.signals import post_save, post_delete, m2m_changed, pre_delete, pre_save
-from django.dispatch import receiver
-from django.db.models import Sum
-from .models import InventoryAsset, Inventory
-from actives.models import Business
-from django.contrib.contenttypes.models import ContentType
-
-
-@receiver(pre_delete, sender=InventoryAsset)
-def update_inventory_total_cost_before_delete(sender, instance, **kwargs):
-    # Проверяем, связан ли asset с каким-либо инвентарем
-    inventory = Inventory.objects.filter(assets=instance).first()
-    if inventory:
-        # Вычитаем стоимость удаляемого asset из total_actives_cost инвентаря
-        inventory.total_actives_cost -= instance.price
-        inventory.save()
-        # Пересчитываем total_worth для связанного бизнеса, если есть
-        # update_business_total_worth(inventory)
-
-
-@receiver(m2m_changed, sender=Inventory.assets.through)
-def recalculate_inventory_total_cost(sender, instance, action, **kwargs):
-    if action in ["post_add", "post_remove", "post_clear"]:
-        instance.total_actives_cost = sum(asset.price for asset in instance.assets.all() if not asset.is_consumables)
-        instance.total_consumables_cost = sum(asset.price for asset in instance.assets.all() if asset.is_consumables)
-
-        instance.save()
-
-        # if instance.content_type and instance.object_id:
-        #     business_model = instance.content_type.model_class()
-        #     if issubclass(business_model, Business):
-        #         business_instance = Business.objects.get(id=instance.object_id)
-        #         update_business_total_worth(instance)
-
-
-# @receiver(post_save, sender=InventoryAsset)
-# @receiver(post_delete, sender=InventoryAsset)
-@receiver(pre_delete, sender=InventoryAsset)
-@receiver(pre_save, sender=InventoryAsset)
-def update_inventory_total_cost(sender, instance, **kwargs):
-    # Предположим, что у InventoryAsset есть ForeignKey к Inventory
-    inventory = instance.inventory
-    if inventory:
-        inventory = Inventory.objects.get(pk=instance.inventory.pk)
-        inventory.total_actives_cost = sum(asset.price for asset in inventory.assets.all() if not asset.is_consumables)
-        inventory.total_consumables_cost = sum(asset.price for asset in inventory.assets.all() if asset.is_consumables)
-        inventory.save()
-        # Вызов функции обновления total_worth для связанного бизнеса
-        # update_business_total_worth(inventory)
-
-
-# def update_business_total_worth(inventory):
-#     if inventory.content_type and inventory.object_id:
-#         # business_model = inventory.content_type.model_class()
-#         # if issubclass(business_model, Business):
-#         business_instance = Business.objects.get(id=inventory.object_id)
-#         inventory = business_instance.equipment
-#         business_instance.total_worth = business_instance.revenue + business_instance.own_funds + business_instance.third_party_tools
-#         business_instance.save()
-
-
-
+# from django.db.models.signals import post_save, post_delete, m2m_changed, pre_delete, pre_save
+# from django.dispatch import receiver
+# from django.db.models import Sum
+# from .models import InventoryAsset, Inventory
+# from actives.models import Business
+# from django.contrib.contenttypes.models import ContentType
+#
+#
+# @receiver(pre_delete, sender=InventoryAsset)
+# def update_inventory_total_cost_before_delete(sender, instance, **kwargs):
+#     # Проверяем, связан ли asset с каким-либо инвентарем
+#     inventory = Inventory.objects.filter(assets=instance).first()
+#     if inventory:
+#         # Вычитаем стоимость удаляемого asset из total_actives_cost инвентаря
+#         inventory.total_actives_cost -= instance.price
+#         inventory.save()
+#         # Пересчитываем total_worth для связанного бизнеса, если есть
+#
+#
+#
+# @receiver(m2m_changed, sender=Inventory.assets.through)
+# def recalculate_inventory_total_cost(sender, instance, action, **kwargs):
+#     if action in ["post_add", "post_remove", "post_clear"]:
+#         instance.total_actives_cost = sum(asset.price for asset in instance.assets.all() if not asset.is_consumables)
+#         instance.total_consumables_cost = sum(asset.price for asset in instance.assets.all() if asset.is_consumables)
+#
+#         instance.save()
+#
+# @receiver(pre_delete, sender=InventoryAsset)
+# @receiver(pre_save, sender=InventoryAsset)
+# def update_inventory_total_cost(sender, instance, **kwargs):
+#     # Предположим, что у InventoryAsset есть ForeignKey к Inventory
+#     inventory = instance.inventory
+#     if inventory:
+#         inventory = Inventory.objects.get(pk=instance.inventory.pk)
+#         inventory.total_actives_cost = sum(asset.price for asset in inventory.assets.all() if not asset.is_consumables)
+#         inventory.total_consumables_cost = sum(asset.price for asset in inventory.assets.all() if asset.is_consumables)
+#         inventory.save()
+#
+#
